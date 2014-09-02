829ccd5cefd08fb14de264161a5fce1f1e107f2b
paperdev7 <paperdev7@gmail.com>
2014-09-03 00:45:17 +0900

    201409030045

--- packages/mobile-menu/public/routes/mobileMenu.js
+++ packages/mobile-menu/public/routes/mobileMenu.js
@@ -2,10 +2,33 @@
 
 angular.module('mean.mobile-menu').config(['$stateProvider',
   function($stateProvider) {
+  	 // Check if the user is connected
+    var checkLoggedin = function($q, $timeout, $http, $location) {
+      // Initialize a new promise
+      var deferred = $q.defer();
+
+      // Make an AJAX call to check if the user is logged in
+      $http.get('/loggedin').success(function(user) {
+        // Authenticated
+        if (user !== '0') $timeout(deferred.resolve);
+
+        // Not Authenticated
+        else {
+          $timeout(deferred.reject);
+          $location.url('/login');
+        }
+      });
+
+      return deferred.promise;
+     };
+
     $stateProvider.state('mobileMenuList', {
       url: '/mobileMenu/list',
       templateUrl: 'mobile-menu/views/list.html',
-      controller:'MobileMenuController'
+      controller:'MobileMenuController',
+      resolve: {
+          loggedin: checkLoggedin
+        }
     });
   }
 ]);
